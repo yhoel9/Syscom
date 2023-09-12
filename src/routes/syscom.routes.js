82499a1fa327a1f79a7c312ee5fb8bb94f9ff173
yhoel Abisaith Pérez <74750182+yhoel9@users.noreply.github.com>
2023-09-12 13:17:42 -0600

    add new category in new route

--- src/routes/syscom.routes.js
+++ src/routes/syscom.routes.js
@@ -1,11 +1,11 @@
 import {Router}  from 'express';
 
-import { categorias } from '../controllers/syscom.controllers';
+import { categorias, categoriasID } from '../controllers/syscom.controllers';
 
 const router = Router();
 
 router.get('/categorias', categorias);
-// router.get('/categorias/:id', categoriasID);
-//comment
+router.get('/categorias/:categoryId', categoriasID);
+
 
 export default router;
