0dbda39499ab6c464984b019eba4c519c23fbad9
yhoel Abisaith Pérez <74750182+yhoel9@users.noreply.github.com>
2023-09-12 13:19:53 -0600

    add the url to find by id

--- src/controllers/syscom.controllers.js
+++ src/controllers/syscom.controllers.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { SYSCOM_API, SYSCOM_API_CLIENT, SYSCOM_API_SECRET,HOST } from "../config";
 
-
+// section of categorias
 export const categorias =  async (req, res )=>{
     try {
         
@@ -33,36 +33,38 @@ export const categorias =  async (req, res )=>{
     }
 
 };
+// obtain the categories from by id
+export const categoriasID = async (req, res)=>{
+    try {
+        const {categoryId}= req.params;
+        const params = new URLSearchParams()
+        params.append("grant_type", "client_credentials")
 
-// export const categoriasID = async (req, res)=>{
-//     try {
-//         const params = new URLSearchParams()
-//         const {id}= req.params;
-//         params.append("grand_type", "client_credentials")
 
-
-//     const {data: {access_token} } = axios.post('https://developers.syscom.mx/oauth/token', params,{
-//         headers:{
-//             'Content-Type': 'application/x-www-form-urlencoded',
-//         },
-//         auth: {
-//             username: SYSCOM_API_CLIENT,
-//             password: SYSCOM_API_SECRET,
-//         }
-//     })
-//     console.log(access_token);
-//     const response = await axios.get(`${SYSCOM_API}/categorias/:id`, 
-//     {
-//         headers:{
-//             Authorization: `Bearer ${access_token}`, 
-//         }
-//     });
-//     // console.log(response.data);
-//     res.json(response.data);
-//     } catch (error) {
-//         return res.status(500).send("Something goes wrong");
-//     }
-// };
+        const {data: {access_token},}= await axios.post('https://developers.syscom.mx/oauth/token', params, {
+            headers:{
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            auth: {
+                username: SYSCOM_API_CLIENT,
+                password: SYSCOM_API_SECRET,
+        }
+        
+    });
+    // console.log(access_token);
+    const response = await axios.get(`${SYSCOM_API}/categorias/${categoryId}`, 
+    {
+        headers:{
+            Authorization: `Bearer ${access_token}`, 
+        }
+    });
+    // console.log(response.data);
+    res.json(response.data);
+    
+    } catch (error) {
+        return res.status(500).send("Something goes wrong");
+    }
+};
 
 //section of carrito
 export const carrito = async (req, res) => {
