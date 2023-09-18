fd194cd3cd5031c7db5ce45bd3895ce6c00b381e
yhoel Abisaith Pérez <74750182+yhoel9@users.noreply.github.com>
2023-09-18 13:05:15 -0600

    add the new routes like brands, products and wishlists

--- src/routes/syscom.routes.js
+++ src/routes/syscom.routes.js
@@ -1,11 +1,26 @@
 import {Router}  from 'express';
 
-import { categorias, categoriasID } from '../controllers/syscom.controllers';
+import { categorias, categoriasID, getMarcas, getMarcasById, getMarcasProducts, getProductos, getUtilities, getbills, wishlists, wishlistsId } from '../controllers/syscom.controllers';
 
 const router = Router();
 
+//routes
+//categories
 router.get('/categorias', categorias);
 router.get('/categorias/:categoryId', categoriasID);
+//brands
+router.get('/marcas', getMarcas);
+router.get('/marcas/:brandId' , getMarcasById);
+router.get('/marcas/:brandId/productos', getMarcasProducts);
+//products
+router.get('/productos', getProductos);
+//money exchange 
+router.get('/tipocambio', getUtilities);
+//wishlists
+router.get('/wishlists', wishlists);
+router.get('/wishlists/:wishlistsId', wishlistsId);
+//bills
+router.get('/facturas', getbills);
 
 
 export default router;
