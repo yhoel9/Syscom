9b633e7cade095a6378885c4daae55bb3da54b00
yhoel Abisaith Pérez <74750182+yhoel9@users.noreply.github.com>
2023-10-02 18:27:53 -0600

    pruebas with function delete, post and put on rest api from syscom on controller

--- src/controllers/syscom.controllers.js
+++ src/controllers/syscom.controllers.js
@@ -410,6 +410,48 @@ export const wishlists = async (req, res) => {
         return res.status(500).send("Something goes wrong");
     }
 };
+//create a new wishlists
+export const newWishlists = async(req, res) => {
+    try {
+        const {nombre, productos} =req.body;
+
+        const params = new URLSearchParams()
+        params.append("grant_type", "client_credentials")
+
+
+        const {data: {access_token},}= await axios.post('https://developers.syscom.mx/oauth/token', params, {
+            headers:{
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            auth: {
+                username: SYSCOM_API_CLIENT,
+                password: SYSCOM_API_SECRET,
+        }
+        
+    });
+    // console.log(access_token);
+    const apiUrl = `${SYSCOM_API}/wishlists`;
+    const createWishlist = {
+        nombre: "new wishlists testing ",
+        productos: [
+            87829,
+            87828,
+            75173
+        ]
+    }
+    const response = await axios.post(apiUrl, createWishlist , 
+    {
+        headers:{
+            Authorization: `Bearer ${access_token}`, 
+        }
+    });
+    // console.log(response.data);
+    res.json(response.data);
+        
+    } catch (error) {
+        return res.status(500).send("Something goes wrong");
+    }
+};
 // section of wishlistsId
 export const wishlistsId = async(req, res ) =>{
     try {
@@ -443,39 +485,489 @@ export const wishlistsId = async(req, res ) =>{
     }
 
 };
+export const deleteWishlists = async (req, res) => {
+    try {
+        const {deleteId}=req.params;
+        const params = new URLSearchParams()
+        params.append("grant_type", "client_credentials")
 
 
-//section of carrito
-//requiere other variables in this section
-// export const carrito = async (req, res) => {
-//     try {
+        const {data: {access_token},}= await axios.post('https://developers.syscom.mx/oauth/token', params, {
+            headers:{
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            auth: {
+                username: SYSCOM_API_CLIENT,
+                password: SYSCOM_API_SECRET,
+        }
         
+    });
+    // console.log(access_token);
+    const response = await axios.delete(`${SYSCOM_API}/wishlists/${deleteId}`, 
+    {
+        headers:{
+            Authorization: `Bearer ${access_token}`, 
+        }
+    });
+    // console.log(response.data);
+    res.json(response.data);
+        
+    } catch (error) {
+        return res.status(500).send("Something goes wrong");
+    }
+};
+export const AggProductWishlist = async (req, res) => {
+    try {
+        const {wishlistsId}= req.params;
+        const {productId}= req.body;
+        const params = new URLSearchParams()
+        params.append("grant_type", "client_credentials")
 
+
+        const {data: {access_token},}= await axios.post('https://developers.syscom.mx/oauth/token', params, {
+            headers:{
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            auth: {
+                username: SYSCOM_API_CLIENT,
+                password: SYSCOM_API_SECRET,
+        }
+        
+    });
+    // console.log(access_token);
+    const response = await axios.post(`${SYSCOM_API}/wishlists/${wishlistsId}/productos/${productId}`, 
+    {
+        headers:{
+            Authorization: `Bearer ${access_token}`, 
+        }
+    });
+     console.log(response.data);
+    res.json(response.data);
+    
+    } catch (error) {
+        return res.status(500).send("Something goes wrong");
+    }
+};
+export const deleteProductWishlist = async (req, res) => {
+    try {
+        const {wishlistsId}= req.params;
+        const {productId}= req.body;
+        const params = new URLSearchParams()
+        params.append("grant_type", "client_credentials")
+
+        const {data: {access_token},}= await axios.post('https://developers.syscom.mx/oauth/token', params, {
+            headers:{
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            auth: {
+                username: SYSCOM_API_CLIENT,
+                password: SYSCOM_API_SECRET,
+        }
+        
+    });
+    // console.log(access_token);
+    const response = await axios.delete(`${SYSCOM_API}/wishlists/${wishlistsId}/productos/${productId}`, 
+    {
+        headers:{
+            Authorization: `Bearer ${access_token}`, 
+        }
+    });
+     console.log(response.data);
+    res.json(response.data);
+    
+    } catch (error) {
+        return res.status(500).send("Something goes wrong");
+    }
+};
+
+
+
+
+// section of carrito
+// requiere other variables in this section
+// export const carritoTrigger = async (req, res) => {
+//     try {
+//         const {tipo_entrega, direccion, metodo_pago, moneda, uso_cfdi} = req.body;
+        
 //         const params = new URLSearchParams()
 //         params.append("grant_type", "client_credentials")
+
 
 //         const {data: {access_token},}= await axios.post('https://developers.syscom.mx/oauth/token', params, {
 //             headers:{
 //                 'Content-Type': 'application/x-www-form-urlencoded',
 //             },
-//             auth:{
+//             auth: {
 //                 username: SYSCOM_API_CLIENT,
 //                 password: SYSCOM_API_SECRET,
 //         }
         
 //     });
-//     const response = await axios.get(`${SYSCOM_API}/categorias`,{
+//     // console.log(access_token);
+//     const response = await axios.get(`${SYSCOM_API}/carrito/generar`, 
+//     {
 //         headers:{
-//             Authorization: `Bearer ${access_token}`,
+//             Authorization: `Bearer ${access_token}`, 
 //         }
 //     });
-    
 //     // console.log(response.data);
-//     return res.json(response.data);
-
+//     res.json(response.data);
         
 //     } catch (error) {
 //         return res.status(500).send("Something goes wrong");
 //     }
-// } 
+// }
+export const carritoAddress = async (req, res)=>{
+    try {
+        const params = new URLSearchParams()
+        params.append("grant_type", "client_credentials")
+
+
+        const {data: {access_token},}= await axios.post('https://developers.syscom.mx/oauth/token', params, {
+            headers:{
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            auth: {
+                username: SYSCOM_API_CLIENT,
+                password: SYSCOM_API_SECRET,
+        }
+        
+    });
+    // console.log(access_token);
+    const response = await axios.get(`${SYSCOM_API}/carrito/direcciones`, 
+    {
+        headers:{
+            Authorization: `Bearer ${access_token}`, 
+        }
+    });
+    // console.log(response.data);
+    res.json(response.data);
+        
+    } catch (error) {
+        return res.status(500).send("Something goes wrong");
+    }
+};//get with same link as funtion down
+
+export const carritoInsertAddress = async (req, res)=>{
+    try {
+        const {atencion_a, calle, colonia, num_ext, num_int, codigo_postal, ciudad, estado, pais, telefono } = req.body;
+        const params = new URLSearchParams()
+        params.append("grant_type", "client_credentials")
+
+
+        const {data: {access_token},}= await axios.post('https://developers.syscom.mx/oauth/token', params, {
+            headers:{
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            auth: {
+                username: SYSCOM_API_CLIENT,
+                password: SYSCOM_API_SECRET,
+        }
+        
+    });
+    // console.log(access_token);
+    const apiUrl = `${SYSCOM_API}/carrito/direcciones`
+    const newaddress = {
+        atencion_a: 'yhoel',
+        calle: 'cima oriente',
+        colonia: 'La cuspide',
+        num_ext: '70',
+        num_int: '16',
+        codigo_postal: '45200',
+        ciudad: 'zapopan',
+        estado: 'JAL',
+        pais: 'MEX',
+        telefono: '3321227074'
+    };
+
+    const response = await axios.post(apiUrl, newaddress, 
+    {
+        headers:{
+            Authorization: `Bearer ${access_token}`, 
+        }
+    });
+    // console.log(response.data);
+    res.json(response.data);
+        
+    } catch (error) {
+        return res.status(500).send("Something goes wrong");
+    }
+};//post
+export const carritoUpdateAddress = async (req, res)=>{
+    try {
+        
+        const params = new URLSearchParams()
+        params.append("grant_type", "client_credentials")
+
+
+        const {data: {access_token},}= await axios.post('https://developers.syscom.mx/oauth/token', params, {
+            headers:{
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            auth: {
+                username: SYSCOM_API_CLIENT,
+                password: SYSCOM_API_SECRET,
+        }
+        
+    });
+    // console.log(access_token);
+    const response = await axios.get(`${SYSCOM_API}/carrito/direcciones`, 
+    {
+        headers:{
+            Authorization: `Bearer ${access_token}`, 
+        }
+    });
+    // console.log(response.data);
+    res.json(response.data);
+        
+    } catch (error) {
+        return res.status(500).send("Something goes wrong");
+    }
+};//post
+export const carritoDeleteAddress = async (req, res)=>{
+    try {
+        const {deleteId} = req.query;
+        const params = new URLSearchParams()
+        params.append("grant_type", "client_credentials")
+
+
+        const {data: {access_token},}= await axios.post('https://developers.syscom.mx/oauth/token', params, {
+            headers:{
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            auth: {
+                username: SYSCOM_API_CLIENT,
+                password: SYSCOM_API_SECRET,
+        }
+        
+    });
+    // console.log(access_token);
+    const response = await axios.delete(`${SYSCOM_API}/carrito/direcciones/?id=${deleteId}`, 
+    {
+        headers:{
+            Authorization: `Bearer ${access_token}`, 
+        }
+    });
+    // console.log(response.data);
+    res.json(response.data);
+        
+    } catch (error) {
+        return res.status(500).send("Something goes wrong");
+    }
+};//delete
+export const carritoCountry = async (req, res)=>{
+    try {
+        const params = new URLSearchParams()
+        params.append("grant_type", "client_credentials")
+
+
+        const {data: {access_token},}= await axios.post('https://developers.syscom.mx/oauth/token', params, {
+            headers:{
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            auth: {
+                username: SYSCOM_API_CLIENT,
+                password: SYSCOM_API_SECRET,
+        }
+        
+    });
+    // console.log(access_token);
+    const response = await axios.get(`${SYSCOM_API}/carrito/paises`, 
+    {
+        headers:{
+            Authorization: `Bearer ${access_token}`, 
+        }
+    });
+    // console.log(response.data);
+    res.json(response.data);
+        
+    } catch (error) {
+        return res.status(500).send("Something goes wrong");
+    }
+};
+export const carritostate = async (req, res)=>{
+    try {
+        const {stateId}= req.params;
+        const params = new URLSearchParams()
+        params.append("grant_type", "client_credentials")
+
+
+        const {data: {access_token},}= await axios.post('https://developers.syscom.mx/oauth/token', params, {
+            headers:{
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            auth: {
+                username: SYSCOM_API_CLIENT,
+                password: SYSCOM_API_SECRET,
+        }
+        
+    });
+    // console.log(access_token);
+    const response = await axios.get(`${SYSCOM_API}/carrito/estados/${stateId}`, 
+    {
+        headers:{
+            Authorization: `Bearer ${access_token}`, 
+        }
+    });
+     console.log(response.data);
+    res.json(response.data);
+    
+    } catch (error) {
+        return res.status(500).send("Something goes wrong");
+    }
+};
+export const carritoneighborhood = async (req, res)=>{
+    try {
+        const {NborhoodId}= req.params;
+        const params = new URLSearchParams()
+        params.append("grant_type", "client_credentials")
+
+
+        const {data: {access_token},}= await axios.post('https://developers.syscom.mx/oauth/token', params, {
+            headers:{
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            auth: {
+                username: SYSCOM_API_CLIENT,
+                password: SYSCOM_API_SECRET,
+        }
+        
+    });
+    // console.log(access_token);
+    const response = await axios.get(`${SYSCOM_API}/carrito/colonias/${NborhoodId}`, 
+    {
+        headers:{
+            Authorization: `Bearer ${access_token}`, 
+        }
+    });
+     console.log(response.data);
+    res.json(response.data);
+    
+    } catch (error) {
+        return res.status(500).send("Something goes wrong");
+    }
+};
+export const carritoPayment = async (req, res)=>{
+    try {
+        const params = new URLSearchParams()
+        params.append("grant_type", "client_credentials")
+
+
+        const {data: {access_token},}= await axios.post('https://developers.syscom.mx/oauth/token', params, {
+            headers:{
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            auth: {
+                username: SYSCOM_API_CLIENT,
+                password: SYSCOM_API_SECRET,
+        }
+        
+    });
+    // console.log(access_token);
+    const response = await axios.get(`${SYSCOM_API}/carrito/pago`, 
+    {
+        headers:{
+            Authorization: `Bearer ${access_token}`, 
+        }
+    });
+    // console.log(response.data);
+    res.json(response.data);
+        
+    } catch (error) {
+        return res.status(500).send("Something goes wrong");
+    }
+};
+export const carritoMovings = async (req, res)=>{
+    try {
+        const params = new URLSearchParams()
+        params.append("grant_type", "client_credentials")
+
+
+        const {data: {access_token},}= await axios.post('https://developers.syscom.mx/oauth/token', params, {
+            headers:{
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            auth: {
+                username: SYSCOM_API_CLIENT,
+                password: SYSCOM_API_SECRET,
+        }
+        
+    });
+    // console.log(access_token);
+    const response = await axios.get(`${SYSCOM_API}/carrito/fleteras`, 
+    {
+        headers:{
+            Authorization: `Bearer ${access_token}`, 
+        }
+    });
+    // console.log(response.data);
+    res.json(response.data);
+        
+    } catch (error) {
+        return res.status(500).send("Something goes wrong");
+    }
+};
+export const carritoBranch = async (req, res)=>{
+    try {
+        const params = new URLSearchParams()
+        params.append("grant_type", "client_credentials")
+
+
+        const {data: {access_token},}= await axios.post('https://developers.syscom.mx/oauth/token', params, {
+            headers:{
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            auth: {
+                username: SYSCOM_API_CLIENT,
+                password: SYSCOM_API_SECRET,
+        }
+        
+    });
+    // console.log(access_token);
+    const response = await axios.get(`${SYSCOM_API}/carrito/sucursales`, 
+    {
+        headers:{
+            Authorization: `Bearer ${access_token}`, 
+        }
+    });
+    // console.log(response.data);
+    res.json(response.data);
+        
+    } catch (error) {
+        return res.status(500).send("Something goes wrong");
+    }
+};
+export const carritoCdfi = async (req, res)=>{
+    try {
+        const params = new URLSearchParams()
+        params.append("grant_type", "client_credentials")
+
+
+        const {data: {access_token},}= await axios.post('https://developers.syscom.mx/oauth/token', params, {
+            headers:{
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            auth: {
+                username: SYSCOM_API_CLIENT,
+                password: SYSCOM_API_SECRET,
+        }
+        
+    });
+    // console.log(access_token);
+    const response = await axios.get(`${SYSCOM_API}/carrito/cfdi`, 
+    {
+        headers:{
+            Authorization: `Bearer ${access_token}`, 
+        }
+    });
+    // console.log(response.data);
+    res.json(response.data);
+        
+    } catch (error) {
+        return res.status(500).send("Something goes wrong");
+    }
+};
+
+
+
 
