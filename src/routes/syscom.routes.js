e4f5ebce7a1cde3fabfcd0dc7680dde627d3f3f9
yhoel Abisaith Pérez <74750182+yhoel9@users.noreply.github.com>
2023-10-02 18:19:58 -0600

    pruebas post, delete and put on rest api syscom

--- src/routes/syscom.routes.js
+++ src/routes/syscom.routes.js
@@ -1,6 +1,6 @@
 import {Router}  from 'express';
 
-import { categorias, categoriasID, getBillsByID, getMarcas, getMarcasById, getMarcasProducts, getProductos, getProductosAccesorios, getProductosById, getProductosRelacionados, getUtilities, getbills, wishlists, wishlistsId } from '../controllers/syscom.controllers';
+import { AggProductWishlist, carritoAddress, carritoBranch, carritoCdfi, carritoCountry, carritoDeleteAddress, carritoInsertAddress, carritoMovings, carritoPayment, carritoTrigger, carritoUpdateAddress, carritoneighborhood, carritostate, categorias, categoriasID, deleteWishlists, getBillsByID, getMarcas, getMarcasById, getMarcasProducts, getProductos, getProductosAccesorios, getProductosById, getProductosRelacionados, getUtilities, getbills, newWishlists, wishlists, wishlistsId } from '../controllers/syscom.controllers';
 
 const router = Router();
 
@@ -21,10 +21,25 @@ router.get('/productos/:productId/accesorios', getProductosAccesorios);
 router.get('/tipocambio', getUtilities);
 //wishlists
 router.get('/wishlists', wishlists);
+router.post('/wishlists', newWishlists);
 router.get('/wishlists/:wishlistsId', wishlistsId);
+router.delete('/wishlists/:deleteId', deleteWishlists);
+router.post('/wishlists/:whislistsId/productos/:productId', AggProductWishlist);
 //bills
 router.get('/facturas', getbills);
 router.get('/facturas/:billsId',getBillsByID);
-
+//carrito
+// router.get('/carrito/generar', carritoTrigger);
+router.get('/carrito/direcciones', carritoAddress);
+router.post('/carrito/direcciones', carritoInsertAddress);
+router.post('/carrito/direcciones/{id}', carritoUpdateAddress);
+router.delete('/carrito/direcciones/', carritoDeleteAddress);
+router.get('/carrito/paises', carritoCountry);
+router.get('/carrito/estados/:stateId', carritostate);
+router.get('/carrito/colonias/:NborhoodId', carritoneighborhood);
+router.get('/carrito/pago', carritoPayment);
+router.get('/carrito/fleteras', carritoMovings);
+router.get('/carrito/sucursales', carritoBranch);
+router.get('/carrito/cfdi', carritoCdfi);
 
 export default router;
